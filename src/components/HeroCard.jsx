41235905761e19694b0e9ae1f0b077e4e6377a5f
Aman Dhakate <amandhakate2703@gmail.com>
2026-04-10 08:57:42 +0530

    complete herocard component

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -1,9 +1,10 @@
 const HeroCard = () => {
   return (
-    <div>
-      <h1>Hero Card</h1>
+    <div className="flex flex-col bg-[rgba(124,58,237,0.1)] border-[rgba(124,58,237,0.3)] rounded-xl p-4 mx-4" >
+      <h3 className=" text-xs mb-1 text-[#a78bfa]">Portfolio Value</h3>
+      <h2 className="text-3xl font-bold">$24,831.50</h2>
+      <h3 className=" text-xs mt-1 text-[#34d399]">+12,04.20 (5.1%) today</h3>
     </div>
   )
 }
-
 export default HeroCard
